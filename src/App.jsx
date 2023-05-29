965e4343df7aff87629e334ce8bd378b924c72be
profsain <husseinimudiking@gmail.com>
2023-05-29 23:08:50 +0100

    created navigationbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import './App.css';
+// import "./App.css";
+import Button from "@mui/material/Button";
+import NavigationBar from "./components/navigation/NavigationBar";
 
 function App() {
   return (
     <>
+      <NavigationBar />
       <div className="App">
         <img src="../images/boctrustlogo.png" alt="" />
         <h1>Welcome to Boctrust Microfinance Bank</h1>
+        <Button variant="contained" color="primary">
+          Hello Button
+        </Button>
       </div>
-
-        
     </>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/navigation/NavigationBar.jsx
@@ -0,0 +1,143 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
+
+const drawerWidth = 240;
+const navItems = [
+  "Home",
+  "About Us",
+  "Our Product",
+  "Apply for Loan",
+  "Support",
+  "Blog",
+  "Contact",
+];
+
+function NavigationBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("");
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    handleDrawerToggle(); // Close the drawer after clicking a menu item
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Boctrust
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton
+              sx={{ textAlign: "center" }}
+              selected={activeLink === item}
+              onClick={() => handleLinkClick(item)}
+            >
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        position="fixed"
+        sx={{ backgroundColor: "#1a237e" }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Buctrust
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item}
+                sx={{
+                  color: activeLink === item ? "#fff" : "#ccc",
+                  marginRight: "16px",
+                  fontSize: activeLink === item ? "18px" : "12px",
+                }}
+                onClick={() => handleLinkClick(item)}
+              >
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 4 }}>
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
+
+NavigationBar.propTypes = {
+  window: PropTypes.func,
+};
+
+export default NavigationBar;
