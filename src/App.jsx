4bad518d149b1834b91aef29a05fbb5bab689bbe
profsain <husseinimudiking@gmail.com>
2023-05-29 17:39:03 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <img src="../images/boctrustlogo.png" alt="" />
+        <h1>Welcome to Boctrust Microfinance Bank</h1>
+      </div>
+
+        
+    </>
+  )
+}
+
+export default App;
