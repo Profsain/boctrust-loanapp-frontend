8a5357b26b57d686932a664c2a8d7f429159acf9
profsain <husseinimudiking@gmail.com>
2023-06-24 23:52:23 +0100

    add recaptcha

--- src/components/loanapplication/loanform/ConfirmField.jsx
+++ src/components/loanapplication/loanform/ConfirmField.jsx
@@ -1,29 +1,38 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
-const ConfirmField = ({ labelName, fieldName, type, values, func }) => {
-    const value = values[fieldName];
-    
-    return (
-        <div>
-        <label htmlFor={fieldName}>{labelName}:</label>
-        <input
-            type={type}
-            name={fieldName}
-            value={value}
-            onChange={(e) => func(fieldName, e)}
-        />
-        </div>
-    );
-}
+const ConfirmField = ({
+  labelName,
+  placeholderText,
+  fieldName,
+  type,
+  values,
+  func,
+}) => {
+  const value = values[fieldName];
+
+  return (
+    <div>
+      <label htmlFor={fieldName}>{labelName}</label>
+      <input
+        type={type}
+        name={fieldName}
+        value={value}
+        placeholder={placeholderText}
+        onChange={(e) => func(fieldName, e)}
+      />
+    </div>
+  );
+};
 
 ConfirmField.propTypes = {
   fieldName: PropTypes.string,
   func: PropTypes.func,
   labelName: PropTypes.string,
+  placeholderText: PropTypes.string,
   type: PropTypes.any,
   values: PropTypes.shape({
-    fieldName: PropTypes.any
-  })
-}
+    fieldName: PropTypes.any,
+  }),
+};
 
 export default ConfirmField;
---
+++ src/components/loanapplication/loanform/CreateAccount.jsx
@@ -0,0 +1,73 @@
+import { useFormikContext } from "formik";
+import ReCAPTCHA from "react-google-recaptcha";
+import ConfirmField from "./ConfirmField";
+import Headline from "../../shared/Headline";
+import "./Form.css";
+
+const CreateAccount = () => {
+  const { values, setFieldValue } = useFormikContext();
+  const handleInputChange = (fieldName, event) => {
+    // Update the field value as the user types
+    setFieldValue(fieldName, event.target.value);
+    console.log("values after change", values.fieldName);
+  };
+
+  const handleRecaptcha = (value) => {
+    console.log("recaptcha value", value);
+  };
+
+  return (
+    <div className="SignUpContainer">
+      <div className="container SignUpInner">
+        <Headline color="#000" text="Create Login / User Details" />
+        <div className="row">
+          <div className="col-sm-12 col-md-8">
+            <ConfirmField
+              placeholderText="Username"
+              fieldName="username"
+              type="text"
+              values={values}
+              func={handleInputChange}
+            />
+            <ConfirmField
+              placeholderText="Password"
+              fieldName="password"
+              type="text"
+              values={values}
+              func={handleInputChange}
+            />
+            <ConfirmField
+              placeholderText="Confirm Password"
+              fieldName="confirmpassword"
+              type="text"
+              values={values}
+              func={handleInputChange}
+            />
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <div>
+              <ReCAPTCHA
+                sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                onChange={handleRecaptcha}
+              />
+            </div>
+
+            <div className="ProceedBtn">
+              <button
+                type="button"
+                // onClick={handleNext}
+                // disabled={isSubmitting}
+                className="BtnAction BtnSecondary"
+                style={{ fontSize: "22px", width: "100%" }}
+              >
+                Create Account
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateAccount;
