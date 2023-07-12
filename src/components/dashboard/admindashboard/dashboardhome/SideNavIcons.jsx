5ad26802478de05344204fef6f61598ee99e55b6
profsain <husseinimudiking@gmail.com>
2023-07-12 09:30:15 +0100

    fix

--- src/components/dashboard/admindashboard/dashboardhome/SideNavIcons.jsx
+++ src/components/dashboard/admindashboard/dashboardhome/SideNavIcons.jsx
@@ -5,66 +5,66 @@ const SideNavIcons = () => {
     <div className="FixSideNav">
       <div className="NavIcons">
         <div className="BLogo SBLogo">
-          <img src="src/assets/images/dlogo.png" alt="boctrust-logo" />
+          <img src="images/dlogo.png" alt="boctrust-logo" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/ddashboard.png" alt="dashboard" />
+          <img src="images/ddashboard.png" alt="dashboard" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dprofile.png" alt="profile" />
+          <img src="images/dprofile.png" alt="profile" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dmyloan.png" alt="loan" />
+          <img src="images/dmyloan.png" alt="loan" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dtransfer.png" alt="transfer" />
+          <img src="images/dtransfer.png" alt="transfer" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dadminacc.png" alt="profile" />
+          <img src="images/dadminacc.png" alt="profile" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dwithdraw.png" alt="withdrawer" />
+          <img src="images/dwithdraw.png" alt="withdrawer" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/daccount.png" alt="account" />
+          <img src="images/daccount.png" alt="account" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dremita.png" alt="remita" />
+          <img src="images/dremita.png" alt="remita" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dreport.png" alt="report" />
+          <img src="images/dreport.png" alt="report" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dmda.png" alt="mda" />
+          <img src="images/dmda.png" alt="mda" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dkyc.png" alt="kyc" />
+          <img src="images/dkyc.png" alt="kyc" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dwebsite.png" alt="setting" />
+          <img src="images/dwebsite.png" alt="setting" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dwithdrawmethod.png" alt="method" />
+          <img src="images/dwithdrawmethod.png" alt="method" />
         </div>
         <div className="IconBox">
-          <img src="src/assets/images/dusermanage.png" alt="setting" />
+          <img src="images/dusermanage.png" alt="setting" />
         </div>
 
         <div className="IconBox">
-          <img src="src/assets/images/dreport.png" alt="report" />
+          <img src="images/dreport.png" alt="report" />
         </div>
       </div>
     </div>
